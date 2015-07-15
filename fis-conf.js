e72f6970d44e45d5801ec7dc85665cf6f2e9bb43
Sighless <hefangshi@gmail.com>
2015-07-15 14:41:17 +0800

    Update fis-conf.js

--- fis-conf.js
+++ fis-conf.js
@@ -1,14 +1,36 @@
-fis.config.set('namespace', '{{-app_name-}}');
+/**
+ * @file FIS 配置
+ * @author
+ */
+
+fis.config.set('namespace', 'home');
 
 // chrome下可以安装插件实现livereload功能
 // https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
 fis.config.set('livereload.port', 35729);
 
-fis.config.set('deploy', {
-    'dev' : {
-        from: '/',
-        to: '/',
-        // yog2 默认的部署入口，使用调试模式启动 yog2 项目后，这个入口就会生效。IP与端口请根据实际情况调整。
-        receiver: 'http://127.0.0.1:8085/yog/upload'
-    }
-});
+if (fis.IS_FIS3) {
+    fis.media('debug').match('*', {
+        optimizer: null,
+        useHash: false,
+        deploy: fis.plugin('http-push', {
+            receiver: 'http://127.0.0.1:8085/yog/upload',
+            to: '/'
+        })
+    });
+    fis.media('debug-prod').match('*', {
+        deploy: fis.plugin('http-push', {
+            receiver: 'http://127.0.0.1:8085/yog/upload',
+            to: '/'
+        })
+    });
+}
+else {
+    fis.config.set('deploy', {
+        debug: {
+            to: '/',
+            // yog2 默认的部署入口，使用调试模式启动 yog2 项目后，这个入口就会生效。IP与端口请根据实际情况调整。
+            receiver: 'http://127.0.0.1:8085/yog/upload'
+        }
+    });
+}
